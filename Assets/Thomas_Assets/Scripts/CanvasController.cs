b34aca1a7a4196ed76582228abf14d4c9d394be3
vanlicht <vanlicht@gmail.com>
2016-12-02 17:36:27 -0800

    Type: Add features
    Add more UI screens from idle to start to selection menu

---
+++ Assets/Thomas_Assets/Scripts/CanvasController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    #region Public Variables
+    public bool isTimedScreen;
+    public float screenTime = 3f;
+    public GameObject targetTimedScreen;
+    #endregion
+
+    #region Private Variables
+    private float startTime;
+    #endregion
+
+    #region Main Methods
+    void Start()
+    {
+        SetupCanvas();
+    }
+
+    void OnEnable()
+    {
+        SetupCanvas();
+    }
+
+    void Update()
+    {
+        if (isTimedScreen)
+        {
+            while(Time.time < startTime + screenTime)
+            {
+                return;
+            }
+            if (targetTimedScreen)
+            {
+                gameObject.SetActive(false);
+                targetTimedScreen.SetActive(true);
+            }
+        }
+    }
+    #endregion
+
+    #region Utility Methods
+    void SetupCanvas()
+    {
+        startTime = Time.time;
+    }
+    #endregion
+}
